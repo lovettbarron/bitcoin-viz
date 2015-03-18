e49bccc0d8f6fc356ff59770f89fc70afdc69674
Reid Williams <rwilliams@sf-m-loaner42.ideo.com>
2015-03-18 10:59:51 -0700

    cleaned up some names and added comments

--- public/javascripts/blockchain.js
+++ public/javascripts/blockchain.js
@@ -1,12 +1,13 @@
 var 
-horzSpace = 4, 
+horzSpace = 4, // space between rectangles
+newRectangleExtraHorzSpace = 5, // extra space for new rectanges.
 vertSpace = 4,
-blockHeight = 25,
-pixelsPerBtc = 10,
-maxWidth = $(window).width() - 20,
+rectangleHeight = 25, 
+pixelsPerBtc = 10, // rectangle width in pixels per bitcoin value in the transaction
+maxWidth = $(window).width() - 20, // max width of a transaction. If we see a txn with value greater than this, we'll just display it with this width.
 minWidth = 1,
-opacityDecayRate = 120000, // millis per opacity. 
-closeIntroDelay = 15000;
+opacityDecayRate = 120000, // millis per opacity unit. How we translate how old a transaction is to how opaque the rectangle fill is.
+closeIntroDelay = 15000; // delay before hiding the intro text.
 
 var enterColor = "#FFFDF3";
 var regularColor = "#D816AB";
@@ -18,61 +19,64 @@ var hashToTxnIndex = {};
 
 var vizSvg;
 
+// set up the websocket to chain's api and ask for callbacks for new-transaction
 txnConn.onopen = function (ev) {
-    var req = {type: "new-transaction", block_chain: "bitcoin"};
-    txnConn.send(JSON.stringify(req));
-  };
+  var req = {type: "new-transaction", block_chain: "bitcoin"};
+  txnConn.send(JSON.stringify(req));
+};
 
-  txnConn.onmessage = function (ev) {
-    var x = JSON.parse(ev.data);
-    // console.dir(x);
-    if (x.payload.transaction) {
-      update(x);
-    }
-  };
+// message handler
+txnConn.onmessage = function (ev) {
+  var x = JSON.parse(ev.data);
+  // console.dir(x);
+  if (x.payload.transaction) {
+    update(x);
+  }
+};
 
-  txnConn.onclose = function (ev) {
-    console.log("txnConn closed");
-    txnConn = new WebSocket("wss://ws.chain.com/v2/notifications");
-  };
+// if the connection gets closed reopen it
+txnConn.onclose = function (ev) {
+  console.log("txnConn closed");
+  txnConn = new WebSocket("wss://ws.chain.com/v2/notifications");
+};
 
-  blockConn.onopen = function (ev) {
-    var req = {type: "new-block", block_chain: "bitcoin"};
-    blockConn.send(JSON.stringify(req));
-  };
+// open another socket to receive updates when there's a new block. It might be possible to do this with a single
+// websocket.
+blockConn.onopen = function (ev) {
+  var req = {type: "new-block", block_chain: "bitcoin"};
+  blockConn.send(JSON.stringify(req));
+};
 
-  blockConn.onmessage = function (ev) {
-    var x = JSON.parse(ev.data);
-    console.dir(x);
-    if (x.payload.block) {
-      updateBlock(x);
-    }
-  };
+blockConn.onmessage = function (ev) {
+  var x = JSON.parse(ev.data);
+  console.dir(x);
+  if (x.payload.block) {
+    updateBlock(x);
+  }
+};
 
-  blockConn.onclose = function (ev) {
-    console.log("blockConn closed");
-    blockConn = new WebSocket("wss://ws.chain.com/v2/notifications");
-  };
+blockConn.onclose = function (ev) {
+  console.log("blockConn closed");
+  blockConn = new WebSocket("wss://ws.chain.com/v2/notifications");
+};
 
-
-
+// initialize a bunch of stuff after body is loaded.
 var bodyLoaded = function() {
-  console.log("loaded");
 
   $("#intro-text").animate({ opacity: 1.0}, 3000);
 
   /* you can see what is going on in the engine with console.log(window.engine) */
+  // set up grid style sheets
   window.engine = new GSS(document);
 
-
-
+  // draw rectangle for the example unconfirmed block in the intro text
   d3.select("#unconfirmed-key")
-  .append("svg").attr("height", blockHeight).attr("width", 500)
+  .append("svg").attr("height", rectangleHeight).attr("width", 500)
   .append("rect")
   .attr("class", "unconfirmed")
   .attr("fill-opacity", 0)
   .attr("x", 0).attr("y", 0)
-  .attr("width", pixelsPerBtc*10).attr("height", blockHeight);
+  .attr("width", pixelsPerBtc*10).attr("height", rectangleHeight);
 
   d3.select("#unconfirmed-key").select("svg")
   .append("text")
@@ -82,12 +86,13 @@ var bodyLoaded = function() {
   .attr("class", "smallType")
   .text("A 10 bitcoin transaction");
 
+  // draw rectangle for the example confirmed block
   d3.select("#confirmed-key")
-    .append("svg").attr("height", blockHeight).attr("width", 500)
-    .append("rect")
-    .attr("class", "confirmed")
-    .attr("x", 0).attr("y", 0)
-    .attr("width", pixelsPerBtc*10).attr("height", blockHeight);
+  .append("svg").attr("height", rectangleHeight).attr("width", 500)
+  .append("rect")
+  .attr("class", "confirmed")
+  .attr("x", 0).attr("y", 0)
+  .attr("width", pixelsPerBtc*10).attr("height", rectangleHeight);
 
   d3.select("#confirmed-key").select("svg")
   .append("text")
@@ -97,44 +102,38 @@ var bodyLoaded = function() {
   .attr("class", "smallType")
   .text("A 10 bitcoin confirmed transaction");
 
-  vizSvg = d3.select(".viz").append("svg")
+  // create the svg canvas where we'll draw the visualization
+  vizSvg = d3.select("#viz-canvas").append("svg")
   .attr("width", $(window).width())
   .attr("height", $(window).height());
 
+  // create vent handler for mouse clicks that shows and hides the intro
   d3.select("body")
   .on("click", function() {
-   console.log("clicked");
    if ($("#intro-text").css("opacity") > 0) {
      $("#intro-text").animate({ opacity: 0}, 1000);
-     $("#intro-cover").animate({ opacity: 0}, 1000);
+     $("#intro-container").animate({ opacity: 0}, 1000);
    } else {
      $("#intro-text").animate({ opacity: 1.0}, 1000);
-     $("#intro-cover").animate({ opacity: 0.5}, 1000);
+     $("#intro-container").animate({ opacity: 0.5}, 1000);
    }
   });
 
-  d3.select("body")
-  .on("tap", function() {
-   console.log("clicked");
-   if ($("#intro-text").css("opacity") > 0) {
-     $("#intro-text").animate({ opacity: 0}, 1000);
-     $("#intro-cover").animate({ opacity: 0}, 1000);
-   } else {
-     $("#intro-text").animate({ opacity: 1.0}, 1000);
-     $("#intro-cover").animate({ opacity: 0.5}, 1000);
-   }
-  });
-
+  // set timeout that hides intro text
   setTimeout(function() {
     $("#intro-text").animate({ opacity: 0}, 1000);
-    $("#intro-cover").animate({ opacity: 0}, 1000);
+    $("#intro-container").animate({ opacity: 0}, 1000);
   }, closeIntroDelay);
 };
 
+/* called when we get a new confirmed block chain block from chain. Gets transaction hashes for all
+transactions in the block. Many of these transactions we'll have already seen when we got chain's transaction
+event. We find any transactions we've already seen using the hashToTxnIndex dictionary, and set those transactions to
+have confirmations = 1, meaning they're now included in a blockchain block. This will make them be drawn as green outlines instead of pink.
+*/
 var updateBlock = function(block) {
   var confirmedTxns = block.payload.block.transaction_hashes;
   _.each(confirmedTxns, function(confirmedTxnHash) {
-    console.dir("updating " + confirmedTxnHash)
     var idx = hashToTxnIndex[confirmedTxnHash];
     if (idx) {
       txns[idx].payload.transaction.confirmations = 1;
@@ -142,6 +141,7 @@ var updateBlock = function(block) {
   });
 };
 
+// convert value of transaction in satoshis to pixel width.
 var getWidthFromSatoshis = function(satoshis) {
   var w = (satoshis / 1e8) * pixelsPerBtc;
   w = (w > maxWidth)? maxWidth : w;
@@ -149,22 +149,33 @@ var getWidthFromSatoshis = function(satoshis) {
   return w;
 }
 
+// draw transactions
 var draw = function(data) {
-  var layoutXYList = getRectXYListFromData(data, $(window).width(), $(window).height, blockHeight, horzSpace, vertSpace);
-  var vizSvgHeight = $(".viz").find("svg").height();
+
+  // get xy coordinates of the rectangle we want to draw for each transaction.
+  var layoutXYList = getRectXYListFromData(data, $(window).width(), $(window).height, rectangleHeight, horzSpace, vertSpace);
+  var vizSvgHeight = $("#viz-canvas").find("svg").height();
+
+  // if the y coordinate of the last transaction is greater than window canvas height, make the canvas bigger.
   if (_.last(layoutXYList).y > vizSvgHeight) {
-    $(".viz").find("svg").height(vizSvgHeight + $(window).height());
+    $("#viz-canvas").find("svg").height(vizSvgHeight + $(window).height());
   }
 
+  // we use d3 to draw
   vizSvg.selectAll("rect")
   .data(data)
   .attr("class", function(d) {
+    // set display class based on whether transaction was confirmed or not
     if (d.payload.transaction.confirmations > 0) {
       return "confirmed";
     } else {
       return "unconfirmed";
     }
   })
+
+  /* for confirmed transactions, we draw rectangles with no fill. For unconfirmed transactions, we have a fill opacity that goes
+  down as the transaction gets older.
+  */
   .attr("fill-opacity", function(d, i) {
     if (d.payload.transaction.confirmations > 0) {
       return 1;
@@ -183,9 +194,9 @@ var draw = function(data) {
   })
   .enter()
   .append("rect")
-  // setting x and y for new items is same as x and y for existing, but we dont' update columnLeft or columnTop
+  // properties of the rectangle for a just added transaction.
   .attr("x", function(d, i) {
-    return layoutXYList[i].x;
+    return layoutXYList[i].x + newRectangleExtraHorzSpace;
   })
   .attr("y", function(d, i) {
     return layoutXYList[i].y;
@@ -193,20 +204,27 @@ var draw = function(data) {
   .attr("width", function(d, i) {
     return getWidthFromSatoshis(d.payload.transaction.amount);
   })
-  .attr("height", blockHeight)
+  .attr("height", rectangleHeight)
+
+  // new rectangles start as a bright white color and fade to pink.
   .attr("fill", enterColor)
   .transition()
   .duration(750)
+  .attr("x", function(d, i) {
+    return layoutXYList[i].x;
+  })
   .attr("fill", regularColor);
+
 };
 
-// given the data set of transactions, returns list of objects with computed x and y properties
+/* Given the data set of transactions, returns list of objects with computed x and y properties. We lay rectangles out left to right
+top to bottom on the screen. If a transaction is too wide to fit on the current row, we start a new row below.
+*/
 var getRectXYListFromData = function(txns, screenWidth, screenHeight, rectHeight, horzSpace, vertSpace) {
   var left = 0;
   var top = 0;
   var list = [];
 
-  // a bit ugly since map refers to state outside the iteree function
   for (var i = 0; i < txns.length; i++) {
     var object = {};
     var width = getWidthFromSatoshis(txns[i].payload.transaction.amount);
@@ -229,9 +247,17 @@ var getRectXYListFromData = function(txns, screenWidth, screenHeight, rectHeight
   return list;
 };
 
+/* Handles new transactions coming over the websocket. Using underscore's throttle because sometimes would see many transactions at once and this was causing problems.
+Throttle ensures that a funciton is only called at most once per x milliseconds (here 100).
+*/
 var update = _.throttle(function(txn) {
+
+  // record when we saw the transaction so we can set rectangle opacity by transaction age.
   txn.pushedAt = new Date();
   var listIdx = txns.push(txn);
+
+  // keep a dictionary of all transactions we've seen keyed by the transaction's own hash value. When we receive a block update, 
+  // we'll use this dictionary to look up transactions we know about, and mark them so they display a different color, meaning they've now been included in a block.
   hashToTxnIndex[txn.payload.transaction.hash] = listIdx - 1; 
   draw(txns);
 }, 100);
